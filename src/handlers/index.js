05f43cf73c65663402bb8699eef7adf6168ddb3f
devarsh10 <devarshshah2608@gmail.com>
2026-04-25 00:24:44 +0530

    feat: add tests and register handlers for tppConsents endpoints

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -39,10 +39,15 @@ const tppAccountsRequestErrorByID = require('./tppAccountsRequest/{ID}/error')
 const tppConsentRequests = require('./tppConsentRequests')
 const tppConsentRequestsId = require('./tppConsentRequests/{ID}')
 const tppConsentRequestsErrorByID = require('./tppConsentRequests/{ID}/error')
+const tppConsents = require('./tppConsents')
+const tppConsentsId = require('./tppConsents/{ID}')
 const health = require('./health')
 
 module.exports = {
   HealthGet: health.get,
+  PostConsents: tppConsents.post,
+  GetConsent: tppConsentsId.get,
+  DeleteConsentByID: tppConsentsId.delete,
   NotifyErrorAccountRequest: tppAccountsRequestErrorByID.put,
   GetAccountRequest: tppAccountsRequestId.get,
   UpdateAccountRequest: tppAccountsRequestId.put,
